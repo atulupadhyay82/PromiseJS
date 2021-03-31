4569953bbe68aaffd6fd913d9d7e3069f77f9bc1
swatigaur123 <gaurcs123@gmail.com>
2021-03-31 17:12:25 -0400

    Handling of rejected promises

--- AJAX/script.js
+++ AJAX/script.js
@@ -3,6 +3,10 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError= function(msg){
+  console.log(msg);
+}
+
 /**
  * Using AJAX CALL 1 -->XMLHttpRequest
  * @param {} country 
@@ -158,6 +162,65 @@ const renderCountry = function (data, className = '') {
  * So here, instead of the callback  hell we have what we call a flat chain of promises.  
  * And this one is again,  very easy to understand and to read.  
  */
+// const getCountryByData= function(country){
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//   .then(response => response.json())
+//   .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour=data[0].borders[0];
+//       if(!neighbour) return;
+//       return  fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
+//   })
+//   .then(response => response.json())
+//   .then(data =>  renderCountry(data,'neighbour'));
+  
+// };
+
+/**
+ * A promise  in which an error happens is a rejected promise. 
+ * 
+ * Handling the rejected promises via error handling callback method-
+ * a second callback function into the then method.  So the first callback function here is always gonna be called 
+ * for the fulfilled promise i.e. or a successful one.  And a second callback  which will be called when the promise 
+ * was rejected.  
+ * And this callback function will be called with an argument  which is basically the error itself.  
+ * 
+ * Example- actually the only way  in which the fetch promise rejects  is when the user loses his internet connection.  
+ */
+
+// const getCountryByData= function(country){
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//   .then(
+//     response => response.json(),
+//     err => alert(err)
+//     )
+//   .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour=data[0].borders[0];
+//       if(!neighbour) return;
+//       return  fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
+//   })
+//   .then(
+//     response => response.json(),
+//     err => alert(err)
+//   )
+//   .then(data =>  renderCountry(data,'neighbour'));
+  
+// };
+
+/**
+ * Handling the rejected promises via catch block
+ * that is a little bit annoying  and so in fact there is a better way of basically handling all these errors globally 
+ * just in one central place.So instead of all of these callback functions here  let's just delete them.  
+ * Instead we can handle all the errors  no matter where they appear in the chain right at the end of the chain by 
+ * adding a catch method.
+ * this catch method here  at the end of the chain will basically catch any errors  
+ * that occur in any place in this whole promise chain  and no matter where that is. 
+ * So errors basically propagate down the chain until they are caught, and only if they're not caught anywhere 
+ *  then we get that Uncaught error  to the console.
+
+ * @param {*} country 
+ */
 const getCountryByData= function(country){
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
   .then(response => response.json())
@@ -168,7 +231,17 @@ const getCountryByData= function(country){
       return  fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
   })
   .then(response => response.json())
-  .then(data =>  renderCountry(data,'neighbour'));
+  .then(data =>  renderCountry(data,'neighbour'))
+  .catch(err => {
+    renderError(`Something went wrong  ${err.message}`);
+  })
+  .finally (()=> {
+    //Make spinning wheel or progress bar disappear from the UI 
+    //And then the callback function that we defined here will always be called whatever happens with the promise. 
+    // So no matter if the promise is fulfilled or rejected  this callback function that we define here  is gonna be called always
+  })
+
+  
 };
 
 getCountryByData('usa');
