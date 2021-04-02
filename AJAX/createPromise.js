483b43769acd14d0a6ff0fa176164811884cff7d
swatigaur123 <gaurcs123@gmail.com>
2021-04-02 09:51:07 -0400

    Promisifying the callback functions

--- AJAX/createPromise.js
+++ AJAX/createPromise.js
@@ -2,7 +2,9 @@
  * A callback used to initialize the promise. This callback is passed two arguments: 
  * a resolve callback used to resolve the promise with a value or the result of another promise, 
  * and a reject callback used to reject the promise with a provided reason or error.
- */
+ * 
+ * 
+//  */
 // const lotteryPromise= new Promise(function(resolve,reject){
 //     if(Math.random() >=0.5){
 //         resolve('You Win');
@@ -28,3 +30,31 @@ const lotteryPromise= new Promise(function(resolve,reject){
 
 lotteryPromise.then(res => console.log(res))
 .catch(err =>console.log(err));
+
+//promisfying setTimeout
+const wait= function(seconds){
+    return new Promise(function(resolve){
+        setTimeout(resolve,seconds*1000);
+    })
+};
+
+wait(1)
+    .then( () =>{
+        console.log("I wait for 1 seconds");
+        return wait(2);
+    })
+    .then( () =>{
+        console.log("I wait for 2 seconds");
+        return wait(3);
+    })
+    .then( () =>{
+        console.log("I wait for 3 seconds");
+        return wait(4);
+    })
+    .then( () =>{ console.log("I wait for 4 seconds"); });
+
+    /**
+     * Easy way to create the promises. so basically this is a static method  on the promise constructor
+     */
+    Promise.resolve("ABC").then(x => console.log(x));
+    Promise.reject(new Error("Problem")).catch(x => console.error(x));
